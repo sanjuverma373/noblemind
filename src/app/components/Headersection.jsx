89291a24283223c02486b1083aa0d60fb0407208
sanjuverma373 <sv0240866@gmail.com>
2024-04-06 09:22:35 +0530

    update

--- src/app/components/Headersection.jsx
+++ src/app/components/Headersection.jsx
@@ -12,7 +12,7 @@ const Headersection = () => {
           <span><Stars /></span>
           <p className=' font-Exo font-normal text-sm md:text-base  text-[#131200]'>AI With Noble Mind</p>
         </div>
-        <h1 className=' font-Outfit font-light text-[50px] md:text-[64px] leading-[57px] md:leading-[76px] text-black max-w-[584px] mb-4'>Inspiring Innovation, Elevating Solutions – <span className=' font-semibold text-5xl text-[40px]'>Noble Mind"</span></h1>
+        <h1 className=' font-Outfit font-light text-[50px] md:text-[64px] leading-[57px] md:leading-[76px] text-black max-w-[584px] mb-4'>Inspiring Innovation, Elevating Solutions - <span className=' font-semibold text-5xl text-[40px]'>Noble Mind"</span></h1>
         <p className=' text-black font-Exo font-normal text-sm md:text-base max-w-[584px]'>At Noble Mind is a forward-thinking AI specialised company dedicated to reshaping the future of how we live, work, and learn. Our mission is to create ground-breaking AI solutions for Healthcare, Education, and beyond, aligning with Saudi Arabia’s Vision 2030.”</p>
       </div>
     </div>
